20e4403b4a9855e61c76bf6929a4067f76be7341
umi <9@umi.cat>
2024-04-15 12:14:43 +0800

    change args, rename key_folder and mark fee_payer as optional

--- src/collect.rs
+++ src/collect.rs
@@ -5,36 +5,43 @@ use solana_sdk::{
     signer::EncodableKey, system_instruction, transaction::Transaction,
     message::Message
 };
-
+use tracing::{error, info};
 use crate::Miner;
 
 #[derive(Parser, Debug, Clone)]
 pub struct CollectArgs {
-    #[arg(long)]
-    pub keypair: String,
+    #[arg(long, help = "The folder that contains all the keys used to collect the remaining balance.")]
+    pub key_folder: String,
 
     #[arg(long)]
     pub beneficiary: Pubkey,
 
-    #[arg(long)]
+    #[arg(long, default_value = "")]
     pub fee_payer: String,
 }
 
 impl Miner {
     pub async fn collect(&self, args: &CollectArgs) {
         let client = Miner::get_client_confirmed(&self.rpc);
-        let accounts = Self::read_keys(&args.keypair);
-        let fee_payer_account =  Keypair::read_from_file(&args.fee_payer).unwrap();
+        let accounts = Self::read_keys(&args.key_folder);
+       
+        let fee_payer_account: Keypair = if (&args.fee_payer).is_empty() {
+            accounts[0].insecure_clone() // sorry for this
+        } else {
+            Keypair::read_from_file(&args.fee_payer).unwrap()
+        };
+
+        info!("use account {} as fee payer", fee_payer_account.pubkey());
         
         let mut instructions = Vec::new();
         let mut signers = Vec::new();
-
+       
         let balance_fee_payer = client
             .get_balance(&fee_payer_account.pubkey())
             .await
             .expect("Failed to get balance");
 
-        println!("Fee payer balance: {}", balance_fee_payer);
+        info!("Fee payer balance: {}", balance_fee_payer);
 
         for keypair in accounts.iter() {
             let pubkey = keypair.pubkey();
